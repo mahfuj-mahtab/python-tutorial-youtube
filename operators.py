7c1ff2826a832933a29ea14cf7cd3a39b9bc8653
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2024-12-19 19:36:11 +0600

    operator part 1

---
+++ operators.py
@@ -0,0 +1,28 @@
+# Arithmetic operators - +,-,*,/,%,**,//
+# Assignment operators - =,!=,+=,-+,*=,/=,%=,//=,**=,
+# Comparison operators - ==, != , >, < , >= , <=
+# Logical operators - and or not 
+# Identity operators - is, is not
+# Membership operators - in, not in
+# Bitwise operators - &,|,^,~,<<,>>
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# print(a ** b)
+# print(a != b)
+# a += 2 # a = a + 2
+# b -= 2 # b = b - 2
+# a *= 2
+a = 5
+b = 5
+print(a == b)
+print(a != b)
+print(a < b)
+print(a > b)
+
+print(a <= b)
+print(a >= b)
