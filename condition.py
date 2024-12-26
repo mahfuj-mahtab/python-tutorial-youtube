c26f2bca000e0d301b59573d6fa5e588c4ac8acf
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2024-12-26 18:08:19 +0600

    condition

---
+++ condition.py
@@ -0,0 +1,26 @@
+# icecream = 50
+# money = 50
+
+# if(icecream <= money):
+#   print('I will buy icecream')
+# else:
+#     print('Return home')
+
+
+# m = 1000
+# n = 1000
+
+# if(m >= n):
+#     print('m is bigger')
+# elif(m == n):
+#     print('they are equal')
+# else:
+#     print('n is bigger')
+
+mark = 99
+if(mark > 80):
+    print('A+')
+elif(mark > 70):
+    print('A')
+else:
+    print('fail')
