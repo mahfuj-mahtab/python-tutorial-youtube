465f6f55fc7945d4610c7720c8d71d16928706f3
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2025-01-07 17:55:32 +0600

    list and tuple

--- condition.py
+++ condition.py
@@ -17,10 +17,49 @@
 # else:
 #     print('n is bigger')
 
-mark = 99
-if(mark > 80):
-    print('A+')
-elif(mark > 70):
-    print('A')
+# mark = 99
+# if(mark > 80):
+#     print('A+')
+# elif(mark > 70):
+#     print('A')
+# else:
+#     print('fail')
+
+
+# if(n < 0):
+#     print('negative')
+# else:
+#     print('positive')
+# n = 0
+
+# if(n < 0):
+#     print('negative')
+# elif(n > 0):
+#     print('positive')
+# else:
+#     print('zero')
+
+# n = 11
+# r = n % 2
+
+# if(r == 0):
+#     print('even')
+# else:
+#     print('odd')
+
+# if(n % 2 == 0):
+#     print('even')
+# else:
+#     print('odd')
+
+n = 50
+
+if(n > 10):
+    if(n < 30):
+        print('n is less than 30')
+    elif(n == 50):
+        print('they are equal')
+    else:
+        print('noo')
 else:
-    print('fail')
+    print('no')
---
+++ list_tuple.py
@@ -0,0 +1,35 @@
+# my_num = 10
+# print(my_num)
+
+# my_list = ['Apple','Banana','Orange']
+# my_marks = [1,2,3,4,5,6]
+# a = [1.5,2.5,4.5]
+# print(a)
+# print(type(a))
+# print(my_marks)
+# print(type(my_marks))
+# print(my_list)
+# print(type(my_list))
+
+# my_list = [1,2,3.5,5.5,'Apple','Orange',True,False,[1,2,3]]
+# my_list = [1,2,3.5,5.5,'Apple','Orange',True,False]
+# print(my_list[-3])
+# print(type(my_list))
+# print(my_list[2:6])
+# print(my_list[-4:-2])
+# my_list.append(100)
+# my_list.insert(2,200)
+# print(my_list)
+# my_list.pop()
+# my_list.remove(5.5)
+# print(my_list)
+my_list = [1,2,3.5,5.5,'Apple','Orange',True,False,1,2,3.5]
+print(my_list)
+my_list[4] = 'Banana'
+print(my_list)
+
+my_tuple = (1,2,3,1,2,3,'Apple','Orange',True,False)
+print(my_tuple[6])
+print(type(my_tuple))
+
+
