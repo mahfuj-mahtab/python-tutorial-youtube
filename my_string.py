18be8ffaa1be8693900a9ad8863a65a65fe111a2
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2025-01-08 16:43:45 +0600

    string

---
+++ my_string.py
@@ -0,0 +1,17 @@
+name = 'mohot@gmail.com'
+new_name = name.split('@')[0]
+print(new_name)
+# print(len(name))
+# print(name.upper())
+# print(name.lower())
+# print(name.replace('Mohot','Mahfuj'))
+# print(name[-4:])
+# print(name[:5])
+# print(name[0:])
+# print(name[:])
+# print(name[2:7])
+# print(type(name))
+
+# print(name[4])
+# print(name[6])
+# print(name[-5])
