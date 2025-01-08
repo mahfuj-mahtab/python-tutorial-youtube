dfa8f46e532cad106ce855634f2ccb39787e45f2
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2025-01-08 16:11:34 +0600

    set and dictionary

---
+++ set_dictionary.py
@@ -0,0 +1,25 @@
+# my_set = {10,20,30,40,50,50,50,'apple','banana','orange'}
+# my_set.add(100)
+# my_set.remove('apple')
+# print(my_set)
+
+my_dictionary = {
+    "name" : "Mohot",
+    "age" : 25,
+    "personal_info" : {
+        "email" : "mohot@gmail.com",
+        'phone' : '0170000000'
+    }
+}
+print(my_dictionary["personal_info"]["email"])
+# my_dictionary['name'] = 'Mahfuj Mahtab'
+# my_dictionary['age'] = 26
+# my_dictionary['website'] = 'thechampcoder.com'
+# my_dictionary.pop("age")
+# my_dictionary.popitem()
+# print(my_dictionary)
+# print(my_dictionary["name"])
+# print(my_dictionary["age"])
+# print(my_dictionary.get("name"))
+# print(my_dictionary)
+# print(len(my_dictionary))
