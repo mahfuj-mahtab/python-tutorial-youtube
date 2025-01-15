f193fd40a47d79fc5ac651eaa4c4aace93ff6b80
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2025-01-15 19:35:04 +0600

    condition and while loop basic

--- condition.py
+++ condition.py
@@ -1,65 +1,26 @@
-# icecream = 50
-# money = 50
-
-# if(icecream <= money):
-#   print('I will buy icecream')
-# else:
-#     print('Return home')
-
-
-# m = 1000
-# n = 1000
-
-# if(m >= n):
-#     print('m is bigger')
-# elif(m == n):
+# a = 50
+# b = 50
+# if(a > b):
+#     print('a is Greater than b')
+# elif(a == b):
+#     print('they are equal')
+# elif(a == b):
+#     print('they are equal')
+# elif(a == b):
+#     print('they are equal')
+# elif(a == b):
 #     print('they are equal')
 # else:
-#     print('n is bigger')
-
-# mark = 99
-# if(mark > 80):
-#     print('A+')
-# elif(mark > 70):
-#     print('A')
-# else:
-#     print('fail')
+#     print('b is greater than a')
 
 
-# if(n < 0):
-#     print('negative')
-# else:
-#     print('positive')
-# n = 0
-
-# if(n < 0):
-#     print('negative')
-# elif(n > 0):
-#     print('positive')
-# else:
-#     print('zero')
-
-# n = 11
-# r = n % 2
-
-# if(r == 0):
-#     print('even')
-# else:
-#     print('odd')
-
-# if(n % 2 == 0):
-#     print('even')
-# else:
-#     print('odd')
-
-n = 50
-
-if(n > 10):
-    if(n < 30):
-        print('n is less than 30')
-    elif(n == 50):
-        print('they are equal')
-    else:
-        print('noo')
+a = 25
+if(a > 20 and a < 30):
+    print('yes')
+else:
+    print('no')
+a = 35
+if(a > 20 or a < 30):
+    print('yes')
 else:
     print('no')
---
+++ whileloop.py
@@ -0,0 +1,14 @@
+# for loop 
+# while loop 
+# i = 1
+# while(i < 21):
+#     if(i == 10):
+#         break
+#     print(i,'Hello World')
+#     i=i+1
+i = 0
+while(i < 21):
+    i=i+1
+    if(i == 10):
+        continue
+    print(i,'Hello World')
