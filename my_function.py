457f5e5842cccb9da7b9b1fab97f9ec073858688
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2025-02-24 09:20:43 +0600

    basic function part 1

---
+++ my_function.py
@@ -0,0 +1,13 @@
+# def helloWorld(name,age,another_name):
+#     print('Hello from function', name)
+#     print('Hello from function', age)
+#     print('Hello from function', another_name)
+
+# helloWorld('mohot',25,'mahfuj')
+# *args
+
+def KidsInfo(*kids):
+    print(kids)
+    print(kids[0])
+
+KidsInfo('mohot','mahfuj','mahtab')
